6e272bc19960188caef356284b5ceb87d44e7b97
Alison Moura <alison.oghino@gmail.com>
2020-06-21 12:39:48 -0400

    Tirando comentários, prints e importações não utilizadas

--- preparer.py
+++ preparer.py
@@ -1,13 +1,7 @@
-import pandas as pd
-
 def prepare(combined_csv):
-    # print(combined_csv)
 
     combined_csv = combined_csv.filter(items=['Estado - Sigla', 'Data da Coleta', 'Valor de Venda', 'Produto'], axis=1)
 
-    #
-    # Percorre o vetor combinado juntando as Bandeiras e Revendas em set's (sem repetições)
-    #
     for i, row in combined_csv.iterrows():
         [dia, mes, ano] = row['Data da Coleta'].split('/')
         valor = str(row['Valor de Venda']).replace(',', '.')
@@ -19,5 +13,4 @@ def prepare(combined_csv):
         combined_csv.loc[i,'Ano'] = ano
         combined_csv.loc[i,'Valor'] = valor
 
-    print(combined_csv)
     return combined_csv
